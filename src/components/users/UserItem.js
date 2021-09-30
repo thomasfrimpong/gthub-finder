6d80fe549636c1bc4e36d2a22ddf9e2a7d62c048
Thomas Frimpong <tfrimpong1987@gmail.com>
2021-09-30 11:28:59 +0000

    component to model user

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -8,19 +8,18 @@ const UserItem = ({ user: { login, avatar_url } }) => {
       <img src={avatar_url} className="round-img" style={{ width: "60px" }} />
       <h3>{login}</h3>
       <div>
-        <button className="btn btn-dark">
+        <div>
           <Link to={`/users/${login}`} className="btn btn-dark btn-sm my-1">
             More
           </Link>
-        </button>
+        </div>
       </div>
     </div>
   );
 };
 
 UserItem.propTypes = {
-  login: PropTypes.string.isRequired,
-  avatar_url: PropTypes.string.isRequired,
+  user: PropTypes.object.isRequired,
 };
 
 export default UserItem;
