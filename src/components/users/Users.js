ab35edea384ad8e0f0c382f05844cf44c6a70474
Thomas Frimpong <tfrimpong1987@gmail.com>
2021-09-30 15:56:40 +0000

    display user details and repos

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,28 +1,28 @@
 import React, { useContext } from "react";
-import PropTypes from "prop-types";
 import UserItem from "./UserItem";
 import GithubContext from "../context/github/GithubContext";
+import Spinner from "../layout/Spinner";
 
 const Users = () => {
   const githubContext = useContext(GithubContext);
-  const { users } = githubContext;
+  const { users, loading } = githubContext;
 
-  return (
-    <div style={userStyle}>
-      {users.map((user) => (
-        <UserItem user={user} key={user.id} />
-      ))}
-    </div>
-  );
+  if (loading) {
+    return <Spinner />;
+  } else {
+    return (
+      <div style={userStyle}>
+        {users.map((user) => (
+          <UserItem user={user} key={user.id} />
+        ))}
+      </div>
+    );
+  }
 };
 const userStyle = {
   display: "grid",
   gridTemplateColumns: "repeat(3,1fr)",
   gridGap: "1rem",
-};
-
-Users.propTypes = {
-  users: PropTypes.array.isRequired,
 };
 
 export default Users;
