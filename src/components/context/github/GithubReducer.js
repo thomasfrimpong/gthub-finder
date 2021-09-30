8d04898f7765887db8daad29bf26566aaa4ee99c
Thomas Frimpong <tfrimpong1987@gmail.com>
2021-09-30 14:35:30 +0000

    reducer functions to get user,clear user,etc

--- src/components/context/github/GithubReducer.js
+++ src/components/context/github/GithubReducer.js
@@ -12,6 +12,12 @@ export default (state, action) => {
   switch (action.type) {
     case SEARCH_USERS:
       return { ...state, users: action.payload, loading: false };
+    case GET_USER:
+      return { ...state, user: action.payload, loading: false };
+    case CLEAR_USERS:
+      return { ...state, users: [], loading: false };
+    case SET_LOADING:
+      return { ...state, loading: true };
     default:
       return state;
   }
